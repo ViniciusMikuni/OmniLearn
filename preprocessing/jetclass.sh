9aa5f34459ef76fce33661ed0e069b963b71e80f
PradyunHebbar <pradyun.hebbar@gmail.com>
2024-08-20 18:50:38 +0200

    Removed unnecessary files

--- preprocessing/jetclass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python preprocess_jetclass.py --sample train --folder /u/phebbar/Work/Datasets/JetClass
-python preprocess_jetclass.py --sample test --folder /u/phebbar/Work/Datasets/JetClass
-python preprocess_jetclass.py --sample val --folder /u/phebbar/Work/Datasets/JetClass
